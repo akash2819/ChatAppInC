7acdf44a542c51dd566b77f8ba444b10227c050d
akash2819 <58558224+akash2819@users.noreply.github.com>
2023-03-14 23:48:21 +0530

    Resolved all bugs (final )
    1. If one client gets disconnect then then whole server terminates 
    2. showing wrong number of client 
    3. Not able to send messages to every client in server

--- client.c
+++ client.c
@@ -29,7 +29,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "170.187.239.244", &serv_addr.sin_addr) <= 0)
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
@@ -93,7 +93,7 @@ void *receive_handler(void *socket_desc)
 
     while(1) {
         valread = read( sock , buffer, 1024);
-        printf("Server message: %s\n", buffer);
+        printf("%s\n", buffer);
     }
 
     return NULL;
